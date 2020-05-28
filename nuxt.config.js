cb93eb62826c37aaf2b8b95409712e1e271b2fb7
Luke Carr <me@lukecarr.dev>
2020-05-28 12:04:12 +0100

    Added favicons
    Signed-off-by: Luke Carr <me@lukecarr.dev>

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,19 +11,64 @@ export default {
       {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1'
+      },
+      {
+        name: 'apple-mobile-web-app-title',
+        content: 'Luke Carr'
+      },
+      {
+        name: 'application-name',
+        content: 'Luke Carr'
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#5c677d'
+      },
+      {
+        name: 'msapplication-config',
+        content: '/icons/browserconfig.xml?v=GvbpmoowYb'
+      },
+      {
+        name: 'theme-color',
+        content: '#5c677d'
       }
     ],
 
     link: [
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      },
-      {
         rel: 'stylesheet',
         href:
           'https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap'
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/icons/apple-touch-icon.png?v=GvbpmoowYb'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/icons/favicon-32x32.png?v=GvbpmoowYb'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/icons/favicon-16x16.png?v=GvbpmoowYb'
+      },
+      {
+        rel: 'manifest',
+        href: '/icons/site.webmanifest?v=GvbpmoowYb'
+      },
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg?v=GvbpmoowYb',
+        color: '#5c677d'
+      },
+      {
+        rel: 'shortcut icon',
+        href: '/icons/favicon.ico?v=GvbpmoowYb'
       }
     ]
   },
